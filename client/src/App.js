c8c7ce254e5cbc05776bbbe01e8464137b3ee38a
ramen4lyfe <do.quan.m@gmail.com>
2023-03-05 02:06:59 -0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeList from './components/EmployeeList'
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<EmployeeList />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

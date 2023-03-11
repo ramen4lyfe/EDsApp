5f417e4815c2235c3475938ecf04f17f8c859845
ramen4lyfe <do.quan.m@gmail.com>
2023-03-11 10:41:36 -0800

    adding more fields to reflect table

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Table, Button } from 'react-bootstrap';
+import moment from 'moment'; // Import Moment.js library
 import EmployeeModal from './modals/EmployeeModal';
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
-useEffect(() => {
-  axios.get("http://localhost:8000/api/employees")
-    .then((response) => {
-            console.log(response.data);
-            setEmployees(response.data);
-        })
-    .catch(err => {
-      console.log(err);
-    });
-}, []);
-
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/employees")
+      .then((response) => {
+        console.log(response.data);
+        setEmployees(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
+
+  // Format the birthday using Moment.js
+  const formatDate = (date) => {
+    return moment(date).format('MMMM DD, YYYY');
+  };
 
   return (
     <div className="container mt-5 mb-5">
@@ -29,25 +34,49 @@ useEffect(() => {
           <Table striped bordered hover>
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Preffered Name</th>
+                <th>Gender Name</th>
                 <th>Birthday</th>
+                <th>Personal Email</th>
+                <th>Cell Phone</th>
+                <th>Title</th>
+                <th>Work Email</th>
+                <th>Hire Date</th>
+                <th>Termination Date</th>
+                <th>Promotion Date</th>
+                <th>Active Status</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               {employees.map((employee,index) => (
                 <tr key={employee._id}>
-                  <td>{employee.name}</td>
-                  <td>{employee.email}</td>
-                  <td>{employee.phone}</td>
-                  <td>{employee.birthday}</td>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.prefferedName}</td>
+                  <td>{employee.genderName}</td>
+                  <td>{formatDate(employee.birthday)}</td>
+                  <td>{employee.personalEmail}</td>
+                  <td>{employee.cellPhone}</td>
+                  <td>{employee.businessTitle}</td>
+                  <td>{employee.workEmail}</td>
+                  <td>{formatDate(employee.hirDate)}</td>
+                  <td>{formatDate(employee.terminationDate)}</td>
+                  <td>{formatDate(employee.promotionDate)}</td>
+                  <td>{employee.isActive}</td>
+                  <td className="d-flex justify-content-center">
+                    <Button variant="warning">
+                      Update Info
+                    </Button>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </Table>
           <Button onClick={handleShow} className="btn-primary mt-3">
-            Add Employee Modal
+            Add New Employee
           </Button>
           <EmployeeModal show={showModal} handleClose={handleClose} />
         </div>

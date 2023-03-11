a3bd706a980fe00a267251504d8e5430f5668a67
ramen4lyfe <do.quan.m@gmail.com>
2023-03-11 10:41:16 -0800

    added more fields

--- server/models/employee.model.js
+++ server/models/employee.model.js
@@ -1,23 +1,58 @@
 const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
-    required: [true, "Name is required"],
+    required: [true, "First Name is required"],
   },
-  email: {
+  lastName: {
+    type: String,
+    required: [true, "Last Name is required"],
+  },
+  prefferedName: {
+    type: String,
+  },
+  genderName: {
+    type: String,
+    required: [true, "Please enter self-identified gender for the employee"],
+  enum: ['He/Him', 'She/Her', 'They/Them'],
+  },
+  birthday: {
+    type: Date,
+    required: [true, "Birthday is required"],
+  },
+  personalEmail: {
     type: String,
     required: [true, "Email is required"],
     unique: true,
     match: [/\S+@\S+\.\S+/, "Invalid email format"],
   },
-  phone: {
+  cellPhone: {
     type: Number,
     required: [true, "Phone number is required"],
   },
-  birthday: {
+  businessTitle: {
+    type: String,
+    required: [true, "Business title is required"],
+    enum: ['Employee', 'Manager'],
+  },
+  workEmail: {
+    type: String,
+    required: [true, "EDs email is required"]
+  },
+  hireDate: {
     type: Date,
-    required: [true, "Birthday is required"],
+    required: [true, "Hire date is required"],
+  },
+  terminationDate: {
+    type: Date,
+  },
+  promotionDate: {
+    type: Date,
+  },
+  isActive:{
+    type: Boolean, 
+    required: [true, "Please indicate employement activity"]
   },
   createdAt: {
     type: Date,

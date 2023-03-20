b7058aec8c32db4b1d4f47baa04368661e627494
ramen4lyfe <do.quan.m@gmail.com>
2023-03-20 00:31:53 -0700

    Updates server to make connections to controllers

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("./config/mongoose.config");
+// require all the routes files to make connection between the routes and the controllers
 require("./routes/employee.routes")(app);
+require("./routes/shift.routes")(app);
+require("./routes/booking.routes")(app);
 app.listen(8000, () => {
     console.log("Listening at Port 8000");
 });

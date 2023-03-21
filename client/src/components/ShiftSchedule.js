4ce4c53372f3d6f636c2777fd3db64186cd8e03d
ramen4lyfe <do.quan.m@gmail.com>
2023-03-21 01:34:34 -0700

    remove auto generated dates

--- client/src/components/ShiftSchedule.js
+++ client/src/components/ShiftSchedule.js
@@ -1,38 +1,19 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Container, Row, Col, Table, Button } from 'react-bootstrap';
-import moment from 'moment';
 import { EmployeeContext } from './context/EmployeeContext';
 import axios from 'axios';
 import CreateWorkScheduleModal from './modals/CreateWorkScheduleModal';
+import moment from 'moment';
+
 
 const ShiftSchedule = () => {
   const { employees, setEmployees } = useContext(EmployeeContext);
 
+  // const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
   const [shifts, setShifts] = useState([]);
-
-  const daysOfWeek = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ];
-
-  const generateDates = () => {
-    const startOfWeek = moment().startOf('week').add(1, 'day'); // Start from Monday
-    const dates = [];
-
-    for (let i = 0; i < 7; i++) {
-      dates.push(moment(startOfWeek).add(i, 'days').format('YYYY-MM-DD'));
-    }
-
-    return dates;
-  };
-
-  const dates = generateDates();
-
+  const [currentDate, setCurrentDate] = useState(new Date().toISOString().substr(0, 10));
   const [showCreateModal, setShowCreateModal] = useState(false);
 
   const handleShowCreateModal = () => {
@@ -43,80 +24,89 @@ const ShiftSchedule = () => {
     setShowCreateModal(false);
   };
 
-  useEffect(() => {
-    const startDate = dates[0];
-    const endDate = dates[dates.length - 1];
+  const handleCreateShift = () => {
+    // update shifts after creating a new shift
     axios
-      .get(
-        // `http://localhost:8000/api/shifts?start_date=${startDate}&end_date=${endDate}`
-        `http://localhost:8000/api/shifts`
-      )
+      .get(`http://localhost:8000/api/shifts?date=${currentDate}`)
       .then((response) => {
         setShifts(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  };
 
-return (
-  <Container>
-    <Row className="mb-2">
-      <Col className="d-flex justify-content-end">
-        <Button variant="primary" onClick={handleShowCreateModal}>
-          Create Shift
-        </Button>
-      </Col>
-    </Row>
-    <Table striped bordered hover responsive>
-      <thead>
-        <tr>
-          <th>Day</th>
-          <th>Date</th>
-          <th>Day Shift PIC</th>
-          <th>Day Shift</th>
-          <th>Evening Shift PIC</th>
-          <th>Evening Shift</th>
-        </tr>
-      </thead>
-      <tbody>
-        {daysOfWeek.map((day, index) => {
-          const currentDate = dates[index];
-          const currentShift = shifts.find(
-            (shift) => moment(shift.date).format('YYYY-MM-DD') === currentDate
-          ) || {};
-          return (
-            <tr key={index}>
-              <td>{day}</td>
-              <td>{currentDate}</td>
-              <td>
-                {currentShift.dayShiftPic ? `${currentShift.dayShiftPic.firstName} ${currentShift.dayShiftPic.lastName}` : ''}
-              </td>
-              <td>
-                {Array.isArray(currentShift.dayShift)
-                  ? currentShift.dayShift
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/shifts?date=${currentDate}`)
+      .then((response) => {
+        setShifts(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currentDate]);
+
+  return (
+    <Container>
+      <Row className="mb-2">
+        <Col className="d-flex justify-content-end">
+          <Button variant="primary" onClick={handleShowCreateModal}>
+            Create Shift
+          </Button>
+        </Col>
+      </Row>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Date</th>
+            <th>Day Shift PIC</th>
+            <th>Day Shift</th>
+            <th>Evening Shift PIC</th>
+            <th>Evening Shift</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shifts.map((shift, index) => {
+            const formattedDate = moment(shift.date).format('YYYY-MM-DD');
+            const day = moment(shift.date).format('dddd');
+            return (
+              <tr key={index}>
+                <td>{day}</td>
+                <td>{formattedDate}</td>
+                <td>
+                  {shift.dayShift.pic ? `${shift.dayShift.pic.firstName} ${shift.dayShift.pic.lastName}` : ''}
+                </td>
+                <td>
+                  {Array.isArray(shift.dayShift.employees)
+                    ? shift.dayShift.employees
                       .map((employee) => `${employee.firstName} ${employee.lastName}`)
-                      .join(', ') : ''}
-              </td>
-              <td>
-                {currentShift.eveningShiftPic ? `${currentShift.eveningShiftPic.firstName} ${currentShift.eveningShiftPic.lastName}` : ''}
-              </td>
-              <td>
-                {Array.isArray(currentShift.eveningShift) ? currentShift.eveningShift
+                      .join(', ')
+                    : ''}
+                </td>
+                <td>
+                  {shift.eveningShift.pic ? `${shift.eveningShift.pic.firstName} ${shift.eveningShift.pic.lastName}` : ''}
+                </td>
+                <td>
+                  {Array.isArray(shift.eveningShift.employees)
+                    ? shift.eveningShift.employees
                       .map((employee) => `${employee.firstName} ${employee.lastName}`)
-                      .join(', ') : ''}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
-    <CreateWorkScheduleModal
-      show={showCreateModal}
-      handleClose={handleCloseCreateModal}
-    />
-  </Container>
-);
+                      .join(', ')
+                    : ''}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <CreateWorkScheduleModal
+        show={showCreateModal}
+        handleClose={handleCloseCreateModal}
+        handleCreateShift={handleCreateShift}
+      />
+    </Container>
+  );
 };
 
 export default ShiftSchedule;

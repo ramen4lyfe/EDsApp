7d8462d2bc7f5edabd600f59e9444be5eb92ae4a
ramen4lyfe <do.quan.m@gmail.com>
2023-04-11 00:58:02 -0700

    alpha code commit 4/11

--- client/src/components/AlphaCodes.js
+++ client/src/components/AlphaCodes.js
@@ -2,12 +2,30 @@ import React, { useEffect, useState } from 'react'
 import PayrollNav from './navigation/payrollNav'
 import { Table, Form, Button, Container, Row, Col, ButtonGroup, InputGroup } from "react-bootstrap";
 import axios from 'axios'
+import CreateAlphaCodeModal from './modals/CreateAlphaCodeModal';
 
 
 
 const AlphaCodes = () => {
     const [alphaCode, setAlphaCode] = useState([])
+    const [description, setDescription] = useState([])
     const [payRate, setPayRate] = useState([])
+    const [show, setShow] = useState(false);
+    const [overtimeRate, setOvertimeRate] = useState([1.5])//overtime rate is 1.5 times pay rate
+    const [doubleTimeRate, setDoubleTimeRate] = useState([2])//double time rate is 2 times pay rate
+    // const [trainingRate, setTrainingRate] = useState([0.5])//training rate is base + 1
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/alphaCodes')
+            .then(res => {
+                setAlphaCode(res.data)
+            }
+            )
+            .catch(err => console.log(err))
+    }, [show,])
 
 
 
@@ -18,8 +36,37 @@ const AlphaCodes = () => {
                     <PayrollNav />
                 </Col>
                 <Col md={10} className="p-3">
-                    <h3>Alpha Codes</h3>
-                    
+                    <Row className='d-flex align-items-center'> 
+                        <Col>
+                            <h3> Alpha Codes</h3>
+                        </Col>
+                        <Col>
+                            <ButtonGroup className="float-right">
+                                <Button variant="primary" onClick={handleShow}>Create Alpha Code</Button>
+                                <CreateAlphaCodeModal show={show} onHide={handleClose} />
+                            </ButtonGroup>
+
+                        </Col>
+                    </Row>
+                    <Table bordered hover size='sm' className="mt-4" >
+                        <thead>
+                            <tr>
+                                <th>Alpha Code</th>
+                                <th>Description</th>
+                                <th>Pay Rate</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {alphaCode.map((alphaCode, index) => (
+                                <tr key={index}>
+                                    <td>{alphaCode.alphaCode}</td>
+                                    <td>{alphaCode.description}</td>
+                                    <td>{alphaCode.payRate}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+
+                    </Table>
                 </Col>
             </Row>
         </Container>

58ea7edbe5269a4ae779b31f216072b4fa12d3fc
ramen4lyfe <do.quan.m@gmail.com>
2023-05-06 10:55:38 -0700

    Styling updates

--- client/src/components/CreateUserAccount.js
+++ client/src/components/CreateUserAccount.js
@@ -1,8 +1,9 @@
 // src/components/CreateUserAccount.js
 import React, { useState } from 'react';
-import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col, InputGroup } from 'react-bootstrap';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
 
 const CreateUserAccount = () => {
@@ -13,12 +14,17 @@ const CreateUserAccount = () => {
         password: '',
     });
 
+    const [showPassword, setShowPassword] = useState(false);
+    
     const navigate = useNavigate();
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormData({ ...formData, [name]: value });
     };
+
+    const handleShowPassword = () => setShowPassword(!showPassword);
+    
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -33,65 +39,134 @@ const CreateUserAccount = () => {
             console.error('An error occurred during account creation:', err);
         }
 
-        console.log('User account created:', formData);
+        console.log('User account created:');
     };
 
     return (
-        <Container>
-            <Row className="justify-content-md-center">
-                <Col md="6">
-                    <h1>Create User Account</h1>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="firstName">
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="firstName"
-                                value={formData.firstName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Form.Group>
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+            <Card>
+                <Card.Body>
+                    <Card.Title className='mb-3'>EDscapade Games</Card.Title>
+                    <Card.Title className='mb-4'>Create your User Account</Card.Title>
 
-                        <Form.Group controlId="lastName">
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="lastName"
-                                value={formData.lastName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Form.Group>
+                    <Card.Text>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className='mb-4'>
+                                <Row className='mb-4'>
+                                    <Col>
+                                        <Form.Control
+                                            type="text"
+                                            name="firstName"
+                                            value={formData.firstName}
+                                            onChange={handleChange}
+                                            required
+                                            placeholder='First Name'
+                                        />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control
+                                            type="text"
+                                            name="lastName"
+                                            value={formData.lastName}
+                                            onChange={handleChange}
+                                            required
+                                            placeholder='Last Name'
+                                        />
+                                    </Col>
+                                </Row>
 
-                        <Form.Group controlId="email">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Form.Group>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    required
+                                    placeholder='Username'
+                                    className='mb-4 email-input'
+                                />
 
-                        <Form.Group controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Create Account
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
+                                <Form.Control
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    required
+                                    placeholder='Password'
+                                    className='mb-4'
+                                />
+                                <Form.Group controlId="showPassword" className="mb-4">
+                                    <Form.Check
+                                        type="checkbox"
+                                        label="Show Password"
+                                        onChange={handleShowPassword}
+                                    />
+                                </Form.Group>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Create Account
+                            </Button>
+                        </Form>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted">Already have an account? <Link to="/login">Login</Link></Card.Footer>
+            </Card>
+            {
+                /* <Row className="justify-content-md-center">
+                                <Col md="6">
+                                    <h1>Create User Account</h1>
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Group controlId="firstName" className='mb-4'>
+                                            <Form.Label>First Name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                name="firstName"
+                                                value={formData.firstName}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </Form.Group>
+            
+                                        <Form.Group controlId="lastName">
+                                            <Form.Label>Last Name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                name="lastName"
+                                                value={formData.lastName}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </Form.Group>
+            
+                                        <Form.Group controlId="email">
+                                            <Form.Label>Email Address</Form.Label>
+                                            <Form.Control
+                                                type="email"
+                                                name="email"
+                                                value={formData.email}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </Form.Group>
+            
+                                        <Form.Group controlId="password">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control
+                                                type="password"
+                                                name="password"
+                                                value={formData.password}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </Form.Group>
+            
+                                        <Button variant="primary" type="submit">
+                                            Create Account
+                                        </Button>
+                                    </Form>
+                                </Col>
+                            </Row> */
+            }
         </Container>
     );
 };
